4279bfa0c21ad6caf748cce722762330085319cb
tell-k <ffk2005@gmail.com>
2015-03-30 18:44:19 +0900

    update notify message.

--- callback_plugins/osx_notify.py
+++ callback_plugins/osx_notify.py
@@ -71,9 +71,9 @@ class CallbackModule(object):
 
     def playbook_on_stats(self, stats):
         summary = stats.summarize('127.0.0.1')
-        status = "Complete!"
+        status = "Complete Provisioning!"
         if summary.get('failures') > 0 or summary.get('unreachable') > 0:
-            status = "Failed!"
+            status = "Failed Provisioning!"
         message = " ".join(["{}={}".format(k, v) for k, v in summary.items()])
 
         cmd = "osascript -e 'display notification \"{}\" with title \"{}\"'"
