8f1424d40de4555b3ac672a356e9c804705056a0
tell-k <ffk2005@gmail.com>
2015-12-13 23:54:55 +0900

    update nodejs spec

--- spec/common/nodejs_spec.rb
+++ spec/common/nodejs_spec.rb
@@ -1,13 +1,21 @@
 require 'spec_helper'
 
 target_versions = [
-  'v0.10.9',
-  'v0.11.9',
-  'v0.12.2'
+  'v4.2.3'
+  'v5.2.0'
 ]
-
 target_versions.each do|version|
   describe file(home_path + '/.nodebrew/node/' + version) do
+    it { should be_directory }
+  end
+end
+
+target_versions = [
+  'v2.5.0'
+  'v3.3.1'
+]
+target_versions.each do|version|
+  describe file(home_path + '/.nodebrew/iojs/' + version) do
     it { should be_directory }
   end
 end
