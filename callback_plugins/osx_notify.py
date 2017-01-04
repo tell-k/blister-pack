52a377c4641ab5bc4616922355cbbe194ad632d0
tell-k <ffk2005@gmail.com>
2017-01-04 17:45:59 +0900

    Update callback_plugins/osx_notify.py for ansible v2

--- callback_plugins/osx_notify.py
+++ callback_plugins/osx_notify.py
@@ -1,73 +1,10 @@
 # -*- coding: utf-8 -*-
 
+from ansible.plugins.callback import CallbackBase
 import subprocess
 
 
-class CallbackModule(object):
-
-    def __init__(self):
-        """ constructor """
-        # self.disabled = True
-
-    def on_any(self, *args, **kwargs):
-        pass
-
-    def runner_on_failed(self, host, res, ignore_errors=False):
-        pass
-
-    def runner_on_ok(self, host, res):
-        pass
-
-    def runner_on_skipped(self, host, item=None):
-        pass
-
-    def runner_on_unreachable(self, host, res):
-        pass
-
-    def runner_on_no_hosts(self):
-        pass
-
-    def runner_on_async_poll(self, host, res, jid, clock):
-        pass
-
-    def runner_on_async_ok(self, host, res, jid):
-        pass
-
-    def runner_on_async_failed(self, host, res, jid):
-        pass
-
-    def playbook_on_start(self):
-        pass
-
-    def playbook_on_notify(self, host, handler):
-        pass
-
-    def playbook_on_no_hosts_matched(self):
-        pass
-
-    def playbook_on_no_hosts_remaining(self):
-        pass
-
-    def playbook_on_task_start(self, name, is_conditional):
-        pass
-
-    def playbook_on_vars_prompt(self, varname, private=True,
-                                prompt=None, encrypt=None,
-                                confirm=False, salt_size=None,
-                                salt=None, default=None):
-        pass
-
-    def playbook_on_setup(self):
-        pass
-
-    def playbook_on_import_for_host(self, host, imported_file):
-        pass
-
-    def playbook_on_not_import_for_host(self, host, missing_file):
-        pass
-
-    def playbook_on_play_start(self, name):
-        pass
+class CallbackModule(CallbackBase):
 
     def playbook_on_stats(self, stats):
         summary = stats.summarize('127.0.0.1')
